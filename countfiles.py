64dc5db43cf8f139da2c5f4d19c128bb68df61fe
Cesar Costa <cesar.costa@nextios.com.br>
2023-05-19 14:30:52 -0300

    final dev

--- countfiles.py
+++ countfiles.py
@@ -2,27 +2,6 @@
 import os 
 from datetime import datetime, date, time
 
-files_to_check_cartoes = [
-    "cart_baseiof.csv",
-    "cart_plan_camp.csv",
-    "cart_rot_combinado.csv",
-    "cart_saldodevmin.csv",
-    "CartoesPAGTOMIN.csv",
-    "CartoesREGRACAMPANHA.csv",
-    "Cartoestxcamp.csv"
-]
-
-files_to_check_matera = [
-    "mat_Cadoc.csv",
-    "mat_CC.csv",
-    "mat_CC_acumulativo.csv",
-    "mat_CC_ANUAL.csv",
-    "mat_CC_atual.csv",
-    "mat_Vers_Matera.csv",
-]
-
-files = []
-
 def lengthEqual(array,number):
     return  number == len(array) 
 
@@ -46,7 +25,8 @@ def get_file_date(file_path) -> datetime:
     }
     return separate_datetime
 
-def count_files(dir: str) -> int: 
+def count_files(dir: str):
+    files = [] 
     count = 0 
     list_dir = list(os.scandir(dir)) 
     for file in list_dir: 
@@ -57,35 +37,54 @@ def count_files(dir: str) -> int:
             file_date = get_file_date(file_path)
             file_object = {"name": file_name, "date": file_date['date'], "time": file_date['time']}
             files.append(file_object)
-    return count
+    return files
 
 args = { 
     'path_cartoes': 'C:\\Users\\cezar\\Documents\\nextios\\lab_folder\\cartoes',
     'path_matera': 'C:\\Users\\cezar\\Documents\\nextios\\lab_folder\\matera'
 }
 directory_cartoes = args['path_cartoes']
-directory_matera = args['path_matera'] 
-try: 
-    file_cartoes = count_files(directory_cartoes)
-    index = 0
-    count_ok = 0
-    for file in files:
-        file_name = file['name']
-        file_date = file['date']
-        file_time = file['time']
-        contain = contains(file_name,files_to_check_cartoes)
-        today = is_today(file_date)
-        on_time = is_on_time(file_time)
-        print(contain, today, on_time)
-        if contain and today and on_time:
-            count_ok = count_ok + 1
-        index = index + 1
-    size = lengthEqual(files_to_check_cartoes,count_ok)
-    print('count',count_ok)
-    print('size',len(files_to_check_cartoes) )
-    if size:
-        print (1)
-    else:
-       print (0) 
-except Exception as e: 
-    print(f'Error: {e}')
+directory_matera = args['path_matera']
+
+def check_folder(directory,check_array):  
+    try: 
+        files = count_files(directory)
+        count_ok = 0
+        for file in files:
+            file_name = file['name']
+            file_date = file['date']
+            file_time = file['time']
+            contain = contains(file_name,check_array)
+            today = is_today(file_date)
+            on_time = is_on_time(file_time)
+            if contain and today and on_time:
+                count_ok += 1
+        isValid = lengthEqual(check_array,count_ok)
+        if isValid:
+            return "1"
+        else:
+            return "0" 
+    except Exception as e: 
+        print(f'Error: {e}')
+
+files_to_check_cartoes = [
+    "cart_baseiof.csv",
+    "cart_plan_camp.csv",
+    "cart_rot_combinado.csv",
+    "cart_saldodevmin.csv",
+    "CartoesPAGTOMIN.csv",
+    "CartoesREGRACAMPANHA.csv",
+    "Cartoestxcamp.csv"
+]
+
+files_to_check_matera = [
+    "mat_Cadoc.csv",
+    "mat_CC.csv",
+    "mat_CC_acumulativo.csv",
+    "mat_CC_ANUAL.csv",
+    "mat_CC_atual.csv",
+    "mat_Vers_Matera.csv",
+]
+       
+print('Cartoes: ', check_folder(directory_cartoes,files_to_check_cartoes))
+print('Matera: ', check_folder(directory_matera,files_to_check_matera))
